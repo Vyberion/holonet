ea2c1b32964913dc51186b344446a8cb0f292b91
Vyberion <vyberion@gmail.com>
2026-06-05 12:15:24 +0100

    key

--- modules/auth/permissions.js
+++ modules/auth/permissions.js
@@ -2,13 +2,12 @@ import { tierAtLeast } from "./profile.js";
 
 export const PAGE_ACCESS = {
   registry: { public: true },
-  registry_reaver_reavers: { division: "reavers", minimumTier: "member" },
+  registry_reavers_home: { division: "reavers", minimumTier: "member" },
   registry_dhg: { division: "dhg", minimumTier: "member" },
   registry_inquisitor: { division: "inquisitors", minimumTier: "member" },
   registry_dreadmaster: { division: "dreadmasters", minimumTier: "member" },
   registry_highrank: { highRankMinimumTier: "lower" },
   index: { public: true },
-  index_reaver_reavers: { division: "reavers", minimumTier: "member" }
 };
 
 const HIGH_RANK_TIERS = ["none", "lower", "upper", "overseer"];

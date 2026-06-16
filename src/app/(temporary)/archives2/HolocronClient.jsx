420c113fa22ef799573404b87d1476f45182ee94
Vyber <86562920+Vyberion@users.noreply.github.com>
2026-06-16 12:48:32 +0100

    chore: group temporary routes

--- src/app/(temporary)/archives2/HolocronClient.jsx
+++ src/app/(temporary)/archives2/HolocronClient.jsx
@@ -3,7 +3,7 @@
 import dynamic from "next/dynamic"
 
 const HolocronViewer = dynamic(
-  () => import("../../components/HolocronViewer.jsx"),
+  () => import("../../../components/HolocronViewer.jsx"),
   {
     ssr: false,
     loading: () => (

051d4825f29e26679dbd7de1f09ae1966bb592fa
Vyberion <vyberion@gmail.com>
2026-06-16 13:25:19 +0100

    config: disable alternative Holonet intro to use standard loader only

--- modules/config/intro.js
+++ modules/config/intro.js
@@ -1,4 +1,1 @@
-// One global switch for the alternative Holonet intro.
-// true = use the Establish Link / video / CRT intro flow.
-// false = use the standard loader only.
-export const HOLONET_ALTERNATIVE_INTRO_ENABLED = true;
+export const HOLONET_ALTERNATIVE_INTRO_ENABLED = false;

dd27c68db2c893e4f141112dd67ed610fc9aace9
Vyber <86562920+Vyberion@users.noreply.github.com>
2026-06-30 02:49:43 +0100

    feature(nav): Add galaxy archive link

--- src/components/HolonetNav.jsx
+++ src/components/HolonetNav.jsx
@@ -131,7 +131,8 @@ export function HolonetNav() {
       prefix: "02",
       label: "Archives",
       dropdown: [
-        { href: "/archives/cots", page: "cots", label: "CoTS" }
+        { href: "/archives/cots", page: "archives-cots", label: "CoTS" },
+        { href: "/archives/galaxy-map", page: "archives-galaxy-map", label: "Galaxy" }
       ]
     },
     { href: "/hierarchy", page: "hierarchy", prefix: "03", label: "Hierarchy", preload: preloadHierarchyImages },

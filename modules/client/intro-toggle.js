79fd163c52363efdb74cd8edcb26d84c4fbff021
Vyber <86562920+Vyberion@users.noreply.github.com>
2026-07-02 19:10:49 +0100

    x

--- modules/client/intro-toggle.js
+++ modules/client/intro-toggle.js
@@ -1,12 +1,1 @@
-import { HOLONET_ALTERNATIVE_INTRO_ENABLED } from "../config/intro.js";
-
-const path = typeof window !== "undefined" ? window.location.pathname : "";
-const isGalaxyPage = path === "/galaxy" || path === "/galaxy/";
-
-if (isGalaxyPage) {
-  window.HOLONET_FORCE_RELEASE_INTRO = true;
-  window.HOLONET_ALTERNATIVE_INTRO_ENABLED = true;
-  await import("./galaxy-intro.js");
-} else {
-  window.HOLONET_ALTERNATIVE_INTRO_ENABLED = HOLONET_ALTERNATIVE_INTRO_ENABLED;
-}
+x
